bb176d3187208d9feb16275963dc0f9c1867fe16
innotuyi <innotuyi67@gmail.com>
2023-05-20 00:14:33 +0200

    implementing product feature

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    
+
+
+    public function getAllProducts() {
+
+
+        $products = Product::all();
+
+        return response()->json($products);
+
+
+    }
+
+    public function AddProduct(Request $request) {
+
+
+
+        //   $validatedData = $request->validate([
+        //     'name' => 'required',
+        //     'description' => 'required|string',
+        //     'quantity' => 'required|integer|min:1',
+        //     'price'=> 'required|integer|min:1',
+        // ]);
+
+
+       return  Product::create([
+
+            'name' => $request->name,
+
+            'description' => $request->description,
+
+            'quantity' => $request->quantity,
+
+            'price'=> $request->price,
+
+
+       ]);
+
+
+    }
+
+
+    public function  UpdateProduct(Request $request, $id) {
+
+
+         $product = Product::find( $id);
+
+         if(!$product) {
+
+            return response()->json(['msg'=>"no product found"]);
+         }
+
+
+
+     return    Product::where('id', $id)->update([
+
+            'name' => $request->name,
+
+            'description' => $request->description,
+
+            'quantity' => $request->quantity,
+
+            'price'=> $request->price,
+            
+        ]);
+
+    }
+
+    public function searchProduct($keyword) {
+
+
+        $products = Product::find('name', $keyword);
+
+        if(!$products) {
+
+            return response()->json(['msg'=>"no product found "]);
+
+
+        }
+
+        return response()->json($products);
+
+
+
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,18 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::get('/products', [ProductController::class, 'getAllProducts']);
+
+Route::post('/products/add', [ProductController::class, 'AddProduct']);
+
+Route::put('/products/update/{id}', [ProductController::class, 'UpdateProduct']);
+
+Route::get('/products/search/{keyword}', [ProductController::class, 'searchProduct']);
+
+
+
+
 
 
 
