a5071711bbe7274c25febe2e7643b407f6590d38
innotuyi <innotuyi67@gmail.com>
2023-05-19 23:10:19 +0200

    creating order item migration

---
+++ database/migrations/2023_05_19_210218_create_order_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('OrderItem', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('Order')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+
+            $table->foreign('product_id')->references('id')->on('Product')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
