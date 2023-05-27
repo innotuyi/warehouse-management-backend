714cc059cc8b91d67c2c2e70a1978e52076537f0
innotuyi <innotuyi67@gmail.com>
2023-05-27 12:36:14 +0200

    adding factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+
+            'name' => fake()->name(),
+            'description'=>fake()->name(),
+            'description'=>fake()->numberBetween(100, 1000000),
+            'quantity'=>fake()->random_int(1,4)
+            
+        ];
+    }
+}
---
+++ database/factories/StockFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Stock>
+ */
+class StockFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+             'quantiy'=>fake()->random_int(1,10),
+             'product_id'=>1
+        ];
+    }
+}
